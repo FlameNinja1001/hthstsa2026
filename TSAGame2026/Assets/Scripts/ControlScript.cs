7f8a01470230541c81d78b5cdc5ddaefb7aff367
Samvarth Raj <114258462+FlameNinja1001@users.noreply.github.com>
2026-01-25 15:43:30 -0500

    movement
    basic platformer

---
+++ TSAGame2026/Assets/Scripts/ControlScript.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlScript : MonoBehaviour
+{
+    PlayerInputActions input;
+    public Vector2 move;  
+
+    public Rigidbody rb;
+
+    public bool jump;
+    public float moveLeftRightSpeed;
+    public float rayLength = 1f;
+
+    public bool isGrounded;
+
+    public float jumpForce = 1f;
+    public float gravity = 10f;
+    void Awake()
+    {
+        input = new PlayerInputActions();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        input.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        input.Player.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity += Vector3.down * gravity * Time.fixedDeltaTime;
+        if (isGrounded && jump)
+        {            
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x,jumpForce,rb.linearVelocity.z);
+        }
+    
+        Debug.DrawRay(transform.position, Vector3.down * rayLength, Color.red);
+
+        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayLength))
+        {
+            
+            if (hit.collider.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        jump = input.Player.Jump.IsPressed();
+        move = input.Player.Move.ReadValue<Vector2>();
+        rb.linearVelocity = new Vector3(move.x * moveLeftRightSpeed, rb.linearVelocity.y,rb.linearVelocity.z);
+    }
+
+}
